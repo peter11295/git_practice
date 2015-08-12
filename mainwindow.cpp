13de315e237007ae36a21c8810cde3932efe7fbe
peter11295 <r04631025@ntu.edu.tw>
2015-08-12 10:43:24 +0800

    ver 2

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,18 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QString str= "hello";
+    qDebug()<<str;
+    str.prepend("haha");
+    qDebug()<<str;
+    str.append("yoyo");
+    qDebug()<<str;
+
+    QString str2="momo";
+    QString str3= str+str2 ;
+    qDebug()<<str3;
+
 }
 
 MainWindow::~MainWindow()
